37002d06d74d186144c76260c4870c73a6da188c
hel <helrori@163.com>
2023-05-24 21:31:59 +0800

    modify

--- common/dsp_mul2.v
+++ common/dsp_mul2.v
@@ -17,21 +17,19 @@ dsp_mul2(
    output   wire  signed   [O_W-1 :0] aout        , 
    output   wire  signed   [O_W-1 :0] bout
 );
+localparam M_W = I_W*2;
+reg signed[M_W-1:0]aout_b;
+reg signed[M_W-1:0]bout_b;
 
-reg signed[24-1:0]aout_b;
-reg signed[24-1:0]bout_b;
-localparam BOUT = 24;
-localparam COUT = 16;
-
-wire    carry_bit0   =  aout_b[BOUT-1] ? ( aout_b[BOUT-(COUT-1)-1-1] & ( |aout_b[BOUT-(COUT-1)-1-1-1:0] ) ) : aout_b[BOUT-(COUT-1)-1-1] ;
-assign  aout         = {aout_b[BOUT-1], aout_b[BOUT-1:BOUT-(COUT-1)-1]} + carry_bit0 ;
-wire    carry_bit1   =  bout_b[BOUT-1] ? ( bout_b[BOUT-(COUT-1)-1-1] & ( |bout_b[BOUT-(COUT-1)-1-1-1:0] ) ) : bout_b[BOUT-(COUT-1)-1-1] ;
-assign  bout         = {bout_b[BOUT-1], bout_b[BOUT-1:BOUT-(COUT-1)-1]} + carry_bit1 ;
+wire    carry_bit0   =  aout_b[M_W-1] ? ( aout_b[M_W-(O_W-1)-1-1] & ( |aout_b[M_W-(O_W-1)-1-1-1:0] ) ) : aout_b[M_W-(O_W-1)-1-1] ;
+assign  aout         = {aout_b[M_W-1], aout_b[M_W-1:M_W-(O_W-1)-1]} + carry_bit0 ;
+wire    carry_bit1   =  bout_b[M_W-1] ? ( bout_b[M_W-(O_W-1)-1-1] & ( |bout_b[M_W-(O_W-1)-1-1-1:0] ) ) : bout_b[M_W-(O_W-1)-1-1] ;
+assign  bout         = {bout_b[M_W-1], bout_b[M_W-1:M_W-(O_W-1)-1]} + carry_bit1 ;
 
 always@(posedge clk or negedge rst_n)begin
     if (!rst_n) begin
-        aout_b  <=  'd0;
-        aout_b  <=  'd0;
+        aout_b  <=  {(M_W){1'd0}};
+        aout_b  <=  {(M_W){1'd0}};
     end else begin
         aout_b  <=  ain * din;
         bout_b  <=  bin * din;
