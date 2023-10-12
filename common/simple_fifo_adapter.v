e27f8a8dd49abd55ace5dd72c27ab8ed6f54ab49
hel <hel@github.com>
2023-10-12 13:25:34 +0800

    modify

---
+++ common/simple_fifo_adapter.v
@@ -0,0 +1,66 @@
+/* 
+*   Name  : First_Word_Fall_Through  FIFO
+*   Origin: 231012
+*   EE    : hel
+*/
+module simple_fifo_adapter
+#(
+    parameter integer DATA_IN_WIDTH     = 16  ,
+    parameter integer DATA_OUT_WIDTH    = 128 ,
+    parameter integer ADDR_WIDTH        = 8   ,// DEEP = 2**ADDR_WIDTH 
+    parameter integer FULL_SLACK        = 1 
+)
+(
+    input   wire                    rst     ,    
+    input   wire                    clk     , 
+
+    input   wire                    wr_ena  ,    
+    input   wire[DATA_IN_WIDTH-1:0] wr_dat  , 
+    output  wire                    wr_full ,
+
+    input   wire                    rd_ena  ,    
+    output  wire[DATA_OUT_WIDTH-1:0]rd_dat  , 
+    output  wire                    rd_empty,
+
+    output  wire[ADDR_WIDTH:0]      rd_dat_cnt  
+);
+// small2big width, adapter => big fifo
+localparam SMALL2BIG_DIV = $clog2(DATA_OUT_WIDTH/DATA_IN_WIDTH);
+wire    almost_full = rd_dat_cnt >= {1'd1,{ADDR_WIDTH{1'd0}}}-FULL_SLACK;
+wire    wr_full_int;
+assign  wr_full = (FULL_SLACK==0)?wr_full_int:almost_full;
+generate 
+genvar j;
+    for ( j=0 ; j<SMALL2BIG_DIV ; j=j+1 ) begin :loop
+        wire dout_vld;
+        wire [DATA_IN_WIDTH*(2**(j+1))-1:0]dout;
+        simple_adapter # (
+            .WIDTH_DIN(DATA_IN_WIDTH*(2**j))
+        )
+        simple_adapter_inst (
+            .clk        (clk),
+            .rstn       (~rst),
+            .last_align (1'd0),
+            .din_vld    (j==0?wr_ena:loop[j-1].dout_vld),
+            .din        (j==0?wr_dat:loop[j-1].dout),
+            .dout_vld   (dout_vld),
+            .dout       (dout)
+        );        
+    end
+endgenerate
+simple_fifo#(
+    .DATA_WIDTH(DATA_OUT_WIDTH),
+    .ADDR_WIDTH(ADDR_WIDTH)
+) simple_fifo_tb(
+    .rst     (rst),    
+    .clk     (clk), 
+    .wr_ena  (loop[SMALL2BIG_DIV-1].dout_vld  ),    
+    .wr_dat  (loop[SMALL2BIG_DIV-1].dout  ), 
+    .wr_full (wr_full_int ),
+    .rd_ena  (rd_ena  ),    
+    .rd_dat  (rd_dat  ), 
+    .rd_empty(rd_empty),
+    .dat_cnt (rd_dat_cnt ) 
+);
+
+endmodule
---
+++ sim/simple_fifo_adapter_tb/simple_fifo_adapter_tb.v
@@ -0,0 +1,109 @@
+`timescale 1ns/ 1ps
+module simple_fifo_adapter_tb();
+parameter DATA_IN_WIDTH=16;
+parameter DATA_OUT_WIDTH=128;
+parameter ADDR_WIDTH=4;
+reg         clk=0;
+reg         rst=1;
+reg         wr_ena=0  ;
+reg   [DATA_IN_WIDTH-1:0]wr_dat=0  ;
+wire        wr_full ;
+reg         rd_ena=0  ;
+wire  [DATA_OUT_WIDTH-1:0]rd_dat  ;
+wire        rd_empty;
+wire  [ADDR_WIDTH:0]  dat_cnt ;
+
+
+
+always #10 clk <= ~clk;
+
+simple_fifo_adapter # (
+    .DATA_IN_WIDTH(DATA_IN_WIDTH),
+    .DATA_OUT_WIDTH(DATA_OUT_WIDTH),
+    .ADDR_WIDTH(ADDR_WIDTH),
+    .FULL_SLACK(1)
+  )
+  simple_fifo_adapter_inst (
+    .rst(rst),
+    .clk(clk),
+    .wr_ena(wr_ena),
+    .wr_dat(wr_dat),
+    .wr_full(wr_full),
+    .rd_ena(rd_ena),
+    .rd_dat(rd_dat),
+    .rd_empty(rd_empty),
+    .rd_dat_cnt(dat_cnt)
+  );
+
+task nop;
+    input integer n;
+    begin repeat(n)begin @(posedge clk) #0; end end
+endtask
+
+task push;
+    input [DATA_IN_WIDTH-1:0]dat;
+    begin
+        wr_ena = 1;
+	wr_dat = dat;
+        @(posedge clk) #0;
+        wr_ena = 0;
+    end
+endtask
+task pop;
+    output [DATA_OUT_WIDTH-1:0]dat;
+    begin
+        rd_ena = 1;
+        @(posedge clk)#0;
+        rd_ena = 0;
+	dat    = rd_dat;
+    end
+endtask
+
+integer i;
+reg[127:0] ret;
+initial begin
+    $dumpfile("wave.vcd");
+    $dumpvars(0,simple_fifo_adapter_tb );
+    #10 rst = 0;
+    @(posedge clk)#0;
+    $display("--test 1");
+    for(i=0;i<128;i=i+1)begin
+        push(i); 
+	$display("push %0x",i);
+    end
+
+    nop(100);
+    for(i=0;i<16;i=i+1)begin
+        pop(ret);
+        $display("pop %32x",ret);
+    end
+
+    
+    $display("--test 2");
+    for(i=0;i<16;i=i+1)begin
+	ret = {$random}%256;
+        push(ret); 
+	$display("push %0x",ret);
+    end
+    nop(100);
+    for(i=0;i<2;i=i+1)begin
+        pop(ret);
+        $display("pop %32x",ret);
+    end
+
+
+    $display("--test 4");
+
+    wr_ena = 1;
+    wr_dat = 9;
+    nop(32);
+    rd_ena = 1;
+    nop(32);
+    wr_ena = 0;
+    @(posedge clk)#0;
+    rd_ena = 0;
+
+    #100;
+    $finish;
+end
+endmodule
